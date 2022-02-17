c683e006bf8a34fc0ebc1c8a5c76092fc802ded1
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2022-02-17 11:43:30 -0400

    feat: validate number

--- src/index.js
+++ src/index.js
@@ -14,23 +14,32 @@ app.use(cors({
 
 app.get("/api/character/:number", async (req, res) => {
     const number = req.params.number;
-    try {
+    if (Number.isNaN(number)) {
+        res.status(400).send({ message: `Should be a number the character "number"` });
+        return;
+    }
+    if (number > 0 && number < 11) {
+        res.status(422).send({ message: "The number should be between 1 to 10" });
+        return;
+    }
 
+    try {
         if (await client.exists(number)) {
             const formattedPersona = JSON.parse(await client.get(number));
             res.status(200).send(formattedPersona);
-        } else {
-            const people = await swapi.getPeopleByNumber(number);
-            const homeworld = (await axios.get(people.homeworld)).data;
-
-            const formattedPersona = {
-                name: people.name,
-                birth_year: people.birth_year,
-                homeworld: homeworld.name
-            };
-            client.set(number, JSON.stringify(formattedPersona));
-            res.status(200).send(formattedPersona);
+            return;
         }
+
+        const people = await swapi.getPeopleByNumber(number);
+        const homeworld = (await axios.get(people.homeworld)).data;
+        const formattedPersona = {
+            name: people.name,
+            birth_year: people.birth_year,
+            homeworld: homeworld.name
+        };
+
+        client.set(number, JSON.stringify(formattedPersona));
+        res.status(200).send(formattedPersona);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
