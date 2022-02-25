f1526316a20073806362bf9560a09e751f94e7eb
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2022-02-25 17:19:29 -0400

    refactor: describe and it texts

--- src/test/api.js
+++ src/test/api.js
@@ -3,6 +3,8 @@ let chaiHttp = require('chai-http')
 let server = require('../app');
 let { connect: connectRedis, disconnect: disconnectRedis } = require('../database/redis');
 let should = chai.should();
+const expect = chai.expect
+
 
 chai.use(chaiHttp);
 
@@ -16,8 +18,9 @@ describe('GET /api/character/:number', function () {
         await disconnectRedis();        
     });
 
-    describe('should return 400 status',function(){
-        it('When :number is a string', function (done) {
+    describe('should return 400 status, when',function(){
+        it(':number is a string', function (done) {
+            
             const myString = 'hello'
             chai.request(server)
                 .get(`/api/character/${myString}`)
@@ -26,7 +29,7 @@ describe('GET /api/character/:number', function () {
                     done();
                 });
         });
-        it('When :number is less than 1', function (done) {
+        it(':number is less than 1', function (done) {
             chai.request(server)
                 .get('/api/character/0')
                 .end((err, res) => {
@@ -34,18 +37,19 @@ describe('GET /api/character/:number', function () {
                     done();
                 });
         });
-        it('When: :number is greater than 10', function (done) {
+        it(':number is greater than 10', function (done) {
             chai.request(server)
                 .get('/api/character/11')
                 .end((err, res) => {
-                    res.should.have.status(400);
+                    expect(res.status).to.equal(400);
+                    // res.should.have.status(400);
                     done();
                 });
         });
         
     });
-    describe('Should return 200 status',function(){
-        it('When :number is a number between 1 to 10', function (done) {
+    describe('Should return 200 status, when',function(){
+        it(':number is a number between 1 to 10', function (done) {
             chai.request(server)
                 .get('/api/character/1')
                 .end((err, res) => {
